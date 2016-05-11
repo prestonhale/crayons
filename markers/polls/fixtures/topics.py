a93e98e9d4715105f4a37f17f30d7af9e8d67b0e
Matthew R. Whitt <mwhitt.w@gmail.com>
2016-05-11 00:01:30 -0400

    make factory persist to db

--- markers/polls/fixtures/topics.py
+++ markers/polls/fixtures/topics.py
@@ -15,6 +15,20 @@ def topic_factory():
                 model = Topic
                 django_get_or_create = ('name',)
 
-        name = factory.LazyAttribute(lambda x: faker.sentence(nb_words=5))
+        name = faker.sentence(nb_words=5)
 
     return TopicFactory
+
+
+@pytest.fixture
+def sample_topic():
+    '''
+    sample usage:
+    > topic1 = sample_topic()
+    > topic2 = sample_topic()
+    > topic3 = sample_topic()
+    > Topic.objects.all().count() # returns 3
+    '''
+
+    name = factory.LazyAttribute(lambda x: faker.sentence(nb_words=5))
+    return topic_factory()(name=name)
