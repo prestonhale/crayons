ed565e930c1f7ab60c2f1cefa16068685a6d0b71
Matthew R. Whitt <mwhitt.w@gmail.com>
2016-05-14 22:37:25 -0400

    add travis, codeclimate, heroku
    NOTE: waiting on preston to add github repo to all three. I just added
    the required files for when we hook everything up

--- markers/voters/models.py
+++ markers/voters/models.py
@@ -11,13 +11,12 @@ class CellCarrier(models.Model):
 
 class Voter(models.Model):
 	phone_regex=RegexValidator(
-			regex=r'^\+?1?\d{9,15}$',
-			 message=
-			 	"""
-				 Phone number must be entered in the format: '+999999999'.
-				 Up to 15 digits allowed.
-				 """
-		)
+            regex=r'^\+?1?\d{9,15}$',
+            message="""
+                 Phone number must be entered in the format: '+999999999'.
+                 Up to 15 digits allowed.
+                 """
+        )
 
 	user = models.ForeignKey(User, related_name='voter')
 	phone = models.CharField(max_length=255, validators=[phone_regex])
